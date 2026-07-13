01babd23aa1d8bf328923dbec0d59eaf61bae8a7
Mhd Yaya <mhdyaya@MacBook-Pro-de-Mhd.local>
2026-07-13 16:55:12 -0400

    fix(activité): enregistre ScreenTimeReportView dans l'interop Fabric (sinon vue vide sous New Arch)

--- ios/ReactNativeStarter/ScreenTimeReportViewManager.m
+++ ios/ReactNativeStarter/ScreenTimeReportViewManager.m
@@ -1,5 +1,18 @@
 #import <React/RCTViewManager.h>
+#import <React/RCTLegacyViewManagerInteropComponentView.h>
 
 @interface RCT_EXTERN_MODULE (ScreenTimeReportViewManager, RCTViewManager)
 RCT_EXPORT_VIEW_PROPERTY(period, NSNumber)
+@end
+
+// Enregistre cette vue legacy dans la couche d'interop Fabric (New Architecture),
+// sinon le composant ne s'affiche pas (carte vide).
+@interface ScreenTimeReportInterop : NSObject
+@end
+@implementation ScreenTimeReportInterop
++ (void)load
+{
+  [RCTLegacyViewManagerInteropComponentView
+      supportLegacyViewManagerWithName:@"ScreenTimeReportView"];
+}
 @end
